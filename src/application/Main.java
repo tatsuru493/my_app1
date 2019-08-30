f4cd4349ada6c46c0b42c929fcbb94532c29b895
tatsuru <49518502+tatsuru493@users.noreply.github.com>
2019-08-30 19:43:09 +0900

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,28 +1,10 @@
 package application;
 
-import java.io.IOException;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.Calendar;
-import java.util.List;
-import java.util.ArrayList;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.animation.TranslateTransition;
-import javafx.util.Duration;
-import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.effect.BlurType;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
-
 
 
 public class Main extends Application {
