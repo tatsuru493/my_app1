883f6f2b90514a1233d8ec16f0c0483ff439e5f7
tatsuru <49518502+tatsuru493@users.noreply.github.com>
2019-09-07 15:29:47 +0900

    Update page1Controller.java

--- src/application/page1Controller.java
+++ src/application/page1Controller.java
@@ -1,19 +1,12 @@
 package application;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
 import javafx.event.ActionEvent;
 
 public class page1Controller {
-
-	Stage setStage = new Stage();
 	
     @FXML
     private ResourceBundle resources;
@@ -32,22 +25,17 @@ public class page1Controller {
 
     @FXML
     void onModeM(ActionEvent event) {
-    	try {
-			AnchorPane root = FXMLLoader.load( getClass().getResource("page2.fxml") );
-			setStage.setScene(new Scene(root));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+    	Main.getInstance().setPage2_10minMode();
     }
 
     @FXML
     void onModeQ(ActionEvent event) {
-
+    	Main.getInstance().setPage2_10Q_Mode();
     }
 
     @FXML
     void onClose(ActionEvent event) {
-
+    	Main.getInstance().closeWindow(event);
     }
 
     @FXML
