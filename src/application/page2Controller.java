7320b412829e5316c912603bf46a4e210669b9b0
tatsuru <49518502+tatsuru493@users.noreply.github.com>
2019-11-30 15:17:07 +0900

    Update page2Controller.java

--- src/application/page2Controller.java
+++ src/application/page2Controller.java
@@ -17,29 +17,29 @@ public class page2Controller implements Initializable {
     private URL location;
 
     @FXML
-    private Label checkLabel;
+    private Label modeCheckLabel;
 
     @FXML
     private Button OK_Button;
 
     @FXML
-    private Button cancelButton_p2;
+    private Button backToPage1Button;
 
     @FXML
-    void onCancel_p2(ActionEvent event) {
+    void onOK_Button(ActionEvent event) {
+    	Main.getInstance().setPage3();
+    }
+    
+    @FXML
+    void onbackToPage1Button(ActionEvent event) {
     	Main.getInstance().backToPage1();
     }
 
     @FXML
-    void onOK(ActionEvent event) {
-    	Main.getInstance().setPage3();
-    }
-
-    @FXML
     void initialize() {
-        assert checkLabel != null : "fx:id=\"checkLabel\" was not injected: check your FXML file 'page2.fxml'.";
+        assert modeCheckLabel != null : "fx:id=\"checkLabel\" was not injected: check your FXML file 'page2.fxml'.";
         assert OK_Button != null : "fx:id=\"OK_Button\" was not injected: check your FXML file 'page2.fxml'.";
-        assert cancelButton_p2 != null : "fx:id=\"cancelButton_p2\" was not injected: check your FXML file 'page2.fxml'.";
+        assert backToPage1Button != null : "fx:id=\"cancelButton_p2\" was not injected: check your FXML file 'page2.fxml'.";
 
     }
     
@@ -47,7 +47,7 @@ public class page2Controller implements Initializable {
     public void initialize(URL loacation, ResourceBundle resources) {
     	//-引数にアクセサメソッドを書くと読みづらくなるため-
     	int mode = Main.getInstance().getMode();
-    	checkLabel.setText(Main.getInstance().checkLabelText(mode));
+    	modeCheckLabel.setText(Main.getInstance().checkLabelText(mode));
     }
     
 }
